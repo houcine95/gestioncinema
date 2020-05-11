4db3a860431b7ee6ce81ce64f19a3578760d154a
ELHOUCINE <houcine.mariami1995@gmail.com>
2020-05-11 17:30:33 +0100

    first commit

---
+++ src/test/java/com/fennec/gestioncinema/GestioncinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fennec.gestioncinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
